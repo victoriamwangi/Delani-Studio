209325cd3156b20299ee4c7f977a77e9cec04b7c
victoria mwangi <victoriamwangi021@gmail.com>
2022-03-12 16:11:35 +0300

    feat: toggle between image and more infor

--- js/script.js
+++ js/script.js
@@ -1,18 +1,39 @@
 $(document).ready(function() {
-    $(".s-icon").click(function() {
-        $(".card-text").slideToggle();
 
-        $(".s-icon").hide();
+    $("#s-icon1").click(function() {
+        $(".txt1").toggle();
+        $("#s-icon1").hide();
     });
-
+    $("#s-icon2").click(function() {
+        $(".txt2").toggle();
+        $("#s-icon2").hide();
+    });
+    $("#s-icon3").click(function() {
+        $(".txt3").toggle();
+        $("#s-icon3").hide();
+    });
+    $(".txt1").click(function() {
+        $("#s-icon1").toggle();
+        $(".txt1").hide();
+    });
+    $(".txt2").click(function() {
+        $("#s-icon2").toggle();
+        $(".txt2").hide();
+    });
+    $(".txt3").click(function() {
+        $("#s-icon3").toggle();
+        $(".txt3").hide();
+    });
     $('.submit').click(function() {
         var userName = $('#username').val();
         var email = $('#email').val();
         var message = $('#message').val();
         $('#exampleModal').show();
         if (userName == "" || email == "" || message == "") {
+            $(".fa-rectangle-xmark").show();
             $('.popmsg').text("Kindly fill in all the fields");
         } else {
+            $('.fa-circle-check').show();
             $('.popmsg').text(userName + " we have received your message. Thank you for reaching out!");
 
         }
