c00bc8bd0938495fa01a443eff712799de13fdcb
victoria mwangi <victoriamwangi021@gmail.com>
2022-03-14 11:19:56 +0300

    st:style portfolio section

--- js/script.js
+++ js/script.js
@@ -42,71 +42,103 @@ $(document).ready(function() {
     $("#back-img1").hover(
         function() {
             $("#infors1").show();
+            $(this).animate({ opacity: 0.7 }, 200);
 
         },
         function() {
             $("#infors1").hide();
+            $(this).animate({ opacity: 1.0 }, 200);
+
+
         }
     );
     $("#back-img2").hover(
         function() {
             $("#infors2").show();
+            $(this).animate({ opacity: 0.7 }, 200);
+
         },
         function() {
             $("#infors2").hide();
+            $(this).animate({ opacity: 1.0 }, 200);
+
         }
     );
 
     $("#back-img3").hover(
         function() {
             $("#infors3").show();
+            $(this).animate({ opacity: 0.7 }, 200);
+
         },
         function() {
             $("#infors3").hide();
+            $(this).animate({ opacity: 1.0 }, 200);
+
         }
     );
 
     $("#back-img4").hover(
         function() {
             $("#infors4").show();
+            $(this).animate({ opacity: 0.7 }, 200);
+
         },
         function() {
             $("#infors4").hide();
+            $(this).animate({ opacity: 1.0 }, 200);
+
         }
     );
     $("#back-img5").hover(
         function() {
             $("#infors5").show();
+            $(this).animate({ opacity: 0.7 }, 200);
+
         },
         function() {
             $("#infors5").hide();
+            $(this).animate({ opacity: 1.0 }, 200);
+
         }
     );
 
     $("#back-img6").hover(
         function() {
             $("#infors6").show();
+            $(this).animate({ opacity: 0.7 }, 200);
+
         },
         function() {
             $("#infors6").hide();
+            $(this).animate({ opacity: 1.0 }, 200);
+
         }
     );
 
     $("#back-img7").hover(
         function() {
             $("#infors7").show();
+            $(this).animate({ opacity: 0.7 }, 200);
+
         },
         function() {
             $("#infors7").hide();
+            $(this).animate({ opacity: 1.0 }, 200);
+
         }
     );
 
     $("#back-img8").hover(
         function() {
             $("#infors8").show();
+            $(this).animate({ opacity: 0.7 }, 200);
+
         },
         function() {
             $("#infors8").hide();
+            $(this).animate({ opacity: 1.0 }, 200);
+
         }
     );
 
